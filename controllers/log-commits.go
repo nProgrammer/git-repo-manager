78056e4c79a033d57ecaf2fbf36366dbfc9569ca
nProgrammer <wagner.norbert277@gmail.com>
2021-07-29 11:55:04 +0200

    added next and previous page

--- controllers/log-commits.go
+++ controllers/log-commits.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"regexp"
@@ -8,7 +9,7 @@ import (
 	"github.com/andlabs/ui"
 )
 
-func LogCommits(logs *ui.Label, path string) *ui.Label {
+func LogCommits(logs *ui.Label, path string) [][]string {
 	os.Chdir(path)
 	out, err := exec.Command("git", "log").Output()
 	if err != nil {
@@ -21,15 +22,35 @@ func LogCommits(logs *ui.Label, path string) *ui.Label {
 
 	i := 0
 
-	var text string
+	var text []string
 
 	for i < len(commits) {
-		text = text + "Commit HEX: " + commits[i].Hex + "\nAuthor: " + commits[i].Author + " " + commits[i].Email +
-			"\nDate of commit: " + commits[i].Day + " " + commits[i].Month + " " + commits[i].Dint + " " + commits[i].Time + " " +
-			commits[i].Year + "\nCommit message: " + commits[i].Message + "\n\n"
+		text = append(text, "Commit HEX: "+commits[i].Hex+"\nAuthor: "+commits[i].Author+" "+commits[i].Email+
+			"\nDate of commit: "+commits[i].Day+" "+commits[i].Month+" "+commits[i].Dint+" "+commits[i].Time+" "+
+			commits[i].Year+"\nCommit message: "+commits[i].Message+"\n\n")
 		i++
 	}
-	logs.SetText(text)
 
-	return logs
+	var cmts [][]string
+
+	var cmt []string
+
+	i = 0  // text[i]
+	j := 0 // cmts[][j]
+	for i < len(text) {
+		if j < 5 {
+			cmt = append(cmt, text[i])
+			j++
+			i++
+			if i == len(text) {
+				cmts = append(cmts, cmt)
+			}
+		} else if j == 5 {
+			j = 0
+			cmts = append(cmts, cmt)
+		} else {
+			fmt.Println("ERROR")
+		}
+	}
+	return cmts
 }
--- main.go
+++ main.go
@@ -1,28 +1,62 @@
 package main
 
+//TODO: Lista commitów
+
 import (
 	"git-manager/controllers"
+	"strings"
 
 	"github.com/andlabs/ui"
 )
 
 func main() {
 	err := ui.Main(func() {
+		page := 0
 		input := ui.NewEntry()
+		nextPage := ui.NewButton("Next Page")
+		prePage := ui.NewButton("Previous Page")
 		button := ui.NewButton("Log repo from path")
 		logs := ui.NewLabel("")
+		errors := ui.NewLabel("")
 		box := ui.NewVerticalBox()
 		box.Append(ui.NewLabel("Enter path to repo:"), false)
 		box.Append(input, false)
 		box.Append(button, false)
+		box.Append(prePage, false)
+		box.Append(nextPage, false)
 		box.Append(logs, false)
-		window := ui.NewWindow("Git Manager", 600, 600, false)
+		box.Append(errors, false)
+		window := ui.NewWindow("Git Manager", 800, 600, false)
 		window.SetMargined(true)
 		window.SetChild(box)
 		button.OnClicked(func(*ui.Button) {
-			logs = controllers.LogCommits(logs, input.Text())
+			// logs = controllers.LogCommits(logs, input.Text())
+			data := controllers.LogCommits(logs, input.Text())
+
+			logs.SetText(strings.Join(data[page], ""))
+
 			// greeting.SetText("Commit HEX: " + data[1] + "\nAuthor: " + data[3] + "\nEmail: " + data[4] + "\nDate of commit: " +
 			// 	data[8] + " " + data[9] + " " + data[10] + " " + data[11] + " " + data[12] + "\nMessage: " + strings.Join(data[19:], " "))
+		})
+		nextPage.OnClicked(func(*ui.Button) {
+			errors.SetText("")
+			data := controllers.LogCommits(logs, input.Text())
+			if page == len(data)-1 {
+				errors.SetText("You've reached last page")
+			} else {
+				page++
+			}
+			logs.SetText(strings.Join(data[page], ""))
+		})
+		prePage.OnClicked(func(*ui.Button) {
+			errors.SetText("")
+			data := controllers.LogCommits(logs, input.Text())
+			if page == 0 {
+				errors.SetText("You've reached first page")
+			} else {
+				page--
+			}
+			logs.SetText(strings.Join(data[page], ""))
 		})
 		window.OnClosing(func(*ui.Window) bool {
 			ui.Quit()
